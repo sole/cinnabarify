67ebacdd1940325a0dca67ddb914b872b6b7400b
sole <sole@mozilla.com>
2017-03-08 15:33:17 +0000

    fiiirssst

---
+++ README.md
@@ -0,0 +1,34 @@
+# cinnabarify
+
+This is a scripted version of the steps you'd follow to get a Mozilla Firefox repository with [git-cinnabar](https://github.com/glandium/git-cinnabar) on it.
+
+Basically, it's like the steps at this page, but automated.
+
+## Usage
+
+```bash
+git clone https://github.com/sole/cinnabarify.git
+cd cinnabarify
+./main.sh /path/to/your/desired/target/directory
+```
+
+For example:
+
+```bash
+./main.sh ~/Firefox-code
+```
+
+will do all sorts of magic to get you a git-enabled copy of Firefox's code that works with mercurial underneath and all sorts of other trickery using `git-cinnabar`
+
+## Limitations
+
+Only tested on a Mac OS environment. It's a Bash script.
+
+See the [issues](https://github.com/sole/cinnabarify/issues).
+
+Please be warned that
+- I do *not* intend to turn this into a general purpose tool
+- I just wrote this to help me and new hires get started, and 
+- if you want new features or different behaviours, you're more than welcome to fork this and work in your own version of this script
+
+Please also note that I will gladly accept pull requests that fix the filed issues.
---
+++ main.sh
@@ -0,0 +1,67 @@
+# TODO: check for git, hg, git-cinnabar to be installed
+# https://github.com/sole/cinnabarify/issues/1
+
+# Check for destination directory or exit
+if [ "$#" -eq 0 ]; then
+	echo "👉 ERROR: Please specify the target directory (i.e. where you want to get a Firefox tree that uses git-cinnabar)"
+	exit;
+fi
+
+# All good, let's do this
+
+DESTINATION=$1
+	
+echo "~~~ Cinnabarifying $DESTINATION... ~~~"
+
+if [ -f $DESTINATION ]; then
+	echo "$DESTINATION already exists, please specify another directory"
+	exit
+fi
+
+# Start by creating empty git repo
+git init $1
+cd $1
+
+# Get native helper for faster operations
+git cinnabar download
+
+# If there's an error, the status code (available in the $? variable) is 1
+# so we'll try to install requests... and grab the native helper again (aghhh)
+if [ $? -ne 0 ]; then
+	echo "Attempting to install requests"
+	pip install requests
+	git cinnabar download
+fi
+
+# Various configs for git-cinnabar to be happy
+git config fetch.prune true
+git config push.default upstream
+git remote add mozilla hg::https://hg.mozilla.org/mozilla-unified -t bookmarks/central
+git remote set-url --push mozilla hg::ssh://hg.mozilla.org/integration/mozilla-inbound
+
+# Setup a remote for the try server:
+git remote add try hg::https://hg.mozilla.org/try
+git config remote.try.skipDefaultUpdate true
+git remote set-url --push try hg::ssh://hg.mozilla.org/try
+git config remote.try.push +HEAD:refs/heads/branches/default/tip
+
+# Update ALL THE REMOTES!!!
+echo "☕️ ☕️ ☕️   About to download a  G I G A N T I C  amount of data!!"
+echo "Better grab a cup of your favourite hot beverage, or go do something else... life is too short to wait for hg pulls to finish!"
+git remote update
+
+sentences=(
+	"Hello!"
+	"$DESTINATION has been cinnabarified."
+	"Now go and enjoy using git instead of hg."
+	"Have a wonderful day!"
+)
+
+for i in "${sentences[@]}"
+do
+	echo $i
+	# TODO detect if 'say' exists before attempting to use it
+	# https://github.com/sole/cinnabarify/issues/2
+	say $i
+done
+
